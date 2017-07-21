a778e0f387cf7772142dec35f01c26c3304eea38
Fredrick Esposito <fredrickesposito@gmail.com>
2017-07-21 12:12:54 -0400

    Changed window size
    Changed widow size to 800 by 470.

--- src/png2nam/Main.java
+++ src/png2nam/Main.java
@@ -1,9 +1,6 @@
 package png2nam;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application
@@ -14,10 +11,7 @@ public class Main extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        png2nam.start(primaryStage);
     }
 
 
