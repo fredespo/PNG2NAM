a85000f69e5e0e25c2e018a2aa4c06e5f29de7ed
Fredrick Esposito <fredrickesposito@gmail.com>
2020-10-04 18:21:12 -0400

    Support Java 9+ with compile and run scripts

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-javac -d bin/ src/png2nam/*.java
+javac -d bin/ -cp lib/jfxrt.jar --module-path "lib\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml src/png2nam/*.java
 pause
--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-java -cp bin/ png2nam.Main
+java -cp bin/ --module-path "lib\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml png2nam.Main
 pause
